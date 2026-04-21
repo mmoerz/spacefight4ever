4f27d41d20ac1f80db9a4e765ca5b0c3ff22bfbf
Michael Moerz <michael@moerz.co.at>
2026-04-22 01:16:00 +0200

    fuck

--- spacefight4ever-lib/src/game/ship/definitions/ship_definition.rs
+++ spacefight4ever-lib/src/game/ship/definitions/ship_definition.rs
@@ -19,6 +19,13 @@ pub struct ShipDefinition {
     pub mass: f32,
 }
 
+impl Copy for ShipDefinition {
+    fn copy(&self) -> Self {
+        ShipDefinition { self.name, self.size, self.mass }
+    }
+}
+
+
 impl NamedDefinition for ShipDefinition {
     fn name(&self) -> &str {
         &self.name
@@ -51,80 +58,85 @@ impl AssetLoader for ShipDefinitionLoader {
     }
 }
 
-/// just an alias to usize for ship ids
-///
-/// usize is safer (no overflow), but it uses up i64 on 64bit systems
-/// u32 uses less memory, better for ECS
-pub type ShipDefinitionId = usize;
+// /// just an alias to usize for ship ids
+// ///
+// /// usize is safer (no overflow), but it uses up i64 on 64bit systems
+// /// u32 uses less memory, better for ECS
+// pub type ShipDefinitionId = usize;
 
-/// Manager for ship definitions.
-/// 
-/// stores ship definitions in an immuteable Vec
-/// 
-/// ship defintions are retrieveable by id and name
-/// 
-/// name lookups use a hashmap for fast retrieval
-#[derive(Asset, TypePath)]
-pub struct ShipDefinitionRepository(pub DefinitionRepository<ShipDefinition>);
+// /// Manager for ship definitions.
+// /// 
+// /// stores ship definitions in an immuteable Vec
+// /// 
+// /// ship defintions are retrieveable by id and name
+// /// 
+// /// name lookups use a hashmap for fast retrieval
+// #[derive(Asset, TypePath)]
+// pub struct ShipDefinitionRepository(pub DefinitionRepository<ShipDefinition>);
 
-impl ShipDefinitionRepository {
+// impl ShipDefinitionRepository {
     
-    pub fn from_vec(defs: Vec<ShipDefinition>) -> Self {
-        Self(DefinitionRepository::from_vec(defs))
-    }
+//     pub fn from_vec(defs: Vec<ShipDefinition>) -> Self {
+//         Self(DefinitionRepository::from_vec(defs))
+//     }
 
-    pub fn get_by_id(&self, id: ShipDefinitionId) -> &ShipDefinition {
-        self.0.get_by_id(id as usize)
-    }
-    pub fn get_by_name(&self, name: &str) -> Option<&ShipDefinition> {
-        self.0.get_by_name(name)
-    }
-    pub fn has_id(&self, id: ShipDefinitionId) -> bool {
-        self.0.has_id(id as usize)
-    }
-    pub fn has_name(&self, name: &str) -> bool {
-        self.0.has_name(name)
-    }
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
-}
+//     pub fn get_by_id(&self, id: ShipDefinitionId) -> &ShipDefinition {
+//         self.0.get_by_id(id as usize)
+//     }
+//     pub fn get_by_name(&self, name: &str) -> Option<&ShipDefinition> {
+//         self.0.get_by_name(name)
+//     }
+//     pub fn has_id(&self, id: ShipDefinitionId) -> bool {
+//         self.0.has_id(id as usize)
+//     }
+//     pub fn has_name(&self, name: &str) -> bool {
+//         self.0.has_name(name)
+//     }
+//     pub fn len(&self) -> usize {
+//         self.0.len()
+//     }
+// }
 
 // custom asset loader for ButtonSkin, 
 // which reads from a RON file and loads the associated texture
-// #[derive(Default, TypePath)]
-// pub struct ShipDefinitionLoader;
+#[derive(Default, TypePath)]
+pub struct ShipDefinitionsLoader;
 
 // ship definition file structure
-// #[derive(Deserialize)]
-// pub struct ShipDefinitionFile {
-//     pub ships: Vec<ShipDefinition>,
-// }
+#[derive(Asset, TypePath, Deserialize)]
+pub struct ShipDefinitionFile {
+    pub ships: Vec<ShipDefinition>,
+}
 
 // /// Implementation of the custom asset loader for `ButtonSkin`
 // /// This loader reads a RON file that specifies the texture atlas and mapping for button states,
 // /// and then loads the associated texture as a Bevy asset.
-// impl AssetLoader for ShipDefinitionLoader {
-//     type Asset = ShipDefinitionRepository;
-//     type Settings = ();
-//     type Error = AssetLoadError;
-//     async fn load(
-//         &self,
-//         reader: &mut dyn Reader,
-//         _settings: &(),
-//         _load_context: &mut LoadContext<'_>,
-//     ) -> Result<Self::Asset, Self::Error> {
-//         let mut bytes = Vec::new();
-//         reader.read_to_end(&mut bytes).await?;
+impl AssetLoader for ShipDefinitionsLoader {
+    type Asset = ShipDefinitionFile;
+    type Settings = ();
+    type Error = AssetLoadError;
+    async fn load(
+        &self,
+        reader: &mut dyn Reader,
+        _settings: &(),
+        load_context: &mut LoadContext<'_>,
+    ) -> Result<Self::Asset, Self::Error> {
+        let mut bytes = Vec::new();
+        reader.read_to_end(&mut bytes).await?;
         
-//         let file: ShipDefinitionFile = ron::de::from_bytes(&bytes)?;
+        let file: ShipDefinitionFile = ron::de::from_bytes(&bytes)?;
 
-//         let repo = ShipDefinitionRepository::from_vec(file.ships);
+        for ship in file.ships {
+            load_context.add_labeled_asset(
+                ship.name.clone(),
+                ship,
+            );
+        }
 
-//         Ok(repo)
-//     }
+        Ok(file)
+    }
 
-//     fn extensions(&self) -> &[&str] {
-//         &["ship.def.ron"]
-//     }
-// }
+    fn extensions(&self) -> &[&str] {
+        &["ship.def.ron"]
+    }
+}
