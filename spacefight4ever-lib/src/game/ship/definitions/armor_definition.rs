f29e5887d8864b67a3929e7daa3ac5fde9497449
Michael Moerz <michael@moerz.co.at>
2026-04-27 21:13:49 +0200

    perfect fix for task to enable debugging

--- spacefight4ever-lib/src/game/ship/definitions/armor_definition.rs
+++ spacefight4ever-lib/src/game/ship/definitions/armor_definition.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-
+/// defines an armor
 #[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct ArmorDefinition {
     pub hitpoints: f32,
